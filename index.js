68eba590e0b6743b3eeb2f1304362a0edea154ae
Igor <iponomarevch@gmail.com>
2026-06-06 12:15:40 +0300

    made changes in master

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 console.log("Hello World");
 
-console.error("Error");
-
-console.log("Hello");
+const userAge = 35;
+if (userAge >= 18) {
+  console.log("Вы совершеннолетний");
+} else console.log("Вы НЕ совершеннолетний");
